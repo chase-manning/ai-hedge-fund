ab0310f985b251b30e588b3f17ef22f022ebb263
Chase Manning <chase@manning.dev>
2025-01-26 21:14:19 +0000

    :bug: fix issue with income statements

--- src/financials/get-income-statements.ts
+++ src/financials/get-income-statements.ts
@@ -6,7 +6,7 @@ const getFinancialStatements = async (
   period: string,
   limit: number
 ) => {
-  const url = `${FIN_DATA_API}/financials/statements?ticker=${ticker}&period=${period}&limit=${limit}`;
+  const url = `${FIN_DATA_API}/financials/income-statements?ticker=${ticker}&period=${period}&limit=${limit}`;
   const response = await fetch(url, getFinDataOptions());
   return await response.json();
 };
